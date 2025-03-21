cc1511513ee0257bc4f00342229d1a3c7b35ae5a
AnushkaFagnekar <89957953+AnushkaFagnekar@users.noreply.github.com>
2025-03-22 02:07:09 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "NetworkWatcherRG"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    resource_group_name  = "pipeline-resources"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "anustoragemf"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "prod-tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
